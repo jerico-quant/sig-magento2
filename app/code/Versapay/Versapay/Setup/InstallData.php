f96b3031503100061e83a064892efdc4c41a215f
Jerico Legaspi Quantilus <jerico.l@quantilus.com>
2025-03-18 00:06:16 +0800

    fix outdated install code for versapay

--- app/code/Versapay/Versapay/Setup/InstallData.php
+++ app/code/Versapay/Versapay/Setup/InstallData.php
@@ -13,46 +13,46 @@ use Magento\Customer\Model\Customer;
 
 class InstallData implements InstallDataInterface
 {
-	private $eavSetupFactory;
+    private $eavSetupFactory;
+    private $eavConfig; // Declare the property here
 
-	public function __construct(EavSetupFactory $eavSetupFactory, Config $eavConfig)
-	{
-		$this->eavSetupFactory = $eavSetupFactory;
-		$this->eavConfig       = $eavConfig;
-	}
+    public function __construct(EavSetupFactory $eavSetupFactory, Config $eavConfig)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->eavConfig       = $eavConfig;
+    }
 
-	public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
-	{
-		$eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
-		$eavSetup->addAttribute(
-			\Magento\Customer\Model\Customer::ENTITY,
-			'versapay_walletid',
-			[
-				'type'         => 'varchar',
-				'label'        => 'Versapay WalletId',
-				'input'        => 'text',
-				'required'     => false,
-				'visible'      => true,
-				'user_defined' => true,
-				'position'     => 999,
-				'system'       => 0,
-			]
-		);
-		$sampleAttribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'versapay_walletid');
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Customer\Model\Customer::ENTITY,
+            'versapay_walletid',
+            [
+                'type'         => 'varchar',
+                'label'        => 'Versapay WalletId',
+                'input'        => 'text',
+                'required'     => false,
+                'visible'      => true,
+                'user_defined' => true,
+                'position'     => 999,
+                'system'       => 0,
+            ]
+        );
+        $sampleAttribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'versapay_walletid');
 
-		// more used_in_forms ['adminhtml_checkout','adminhtml_customer','adminhtml_customer_address','customer_account_edit','customer_address_edit','customer_register_address']
-		$sampleAttribute->setData(
-			'used_in_forms',
-			['adminhtml_customer']
+        // more used_in_forms ['adminhtml_checkout','adminhtml_customer','adminhtml_customer_address','customer_account_edit','customer_address_edit','customer_register_address']
+        $sampleAttribute->setData(
+            'used_in_forms',
+            ['adminhtml_customer']
+        );
+        $sampleAttribute->save();
 
-		);
-		$sampleAttribute->save();
-
-		$eavSetup->addAttributeToSet(
+        $eavSetup->addAttributeToSet(
             CustomerMetadataInterface::ENTITY_TYPE_CUSTOMER,
             CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
             null,
-            'versapay_walletid');
-
-	}
+            'versapay_walletid'
+        );
+    }
 }
